9d0c764c9b1f2784d6cdf1c815bbe20f528a393b
eScape LLC <winker9@hotmail.com>
2023-02-01 10:56:01 -0500

    refactor.

--- eScapeLLC.UWP.Charts.Composition/Support/MappingSupport.cs
+++ eScapeLLC.UWP.Charts.Composition/Support/MappingSupport.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace eScapeLLC.UWP.Charts.Composition {
 	public static class MappingSupport {
@@ -11,18 +7,19 @@ namespace eScapeLLC.UWP.Charts.Composition {
 		/// Horizontal axis receives the X, Vertical axis receives the Y.
 		/// </summary>
 		/// <param name="c1">Component 1.</param>
-		/// <param name="c2">Component 2.</param>
 		/// <param name="c1ori">Component 1 axis orientation.</param>
+		/// <param name="c2">Component 2.</param>
 		/// <param name="c2ori">Component 2 axis orientation.</param>
 		/// <returns></returns>
-		/// <exception cref="InvalidOperationException"></exception>
-		public static (double x, double y) MapComponents(double c1, double c2, AxisOrientation c1ori, AxisOrientation c2ori) {
+		/// <exception cref="ArgumentException">Orientations are equal</exception>
+		/// <exception cref="InvalidOperationException">Cannot map components</exception>
+		public static (double xx, double yy) MapComponents(double c1, AxisOrientation c1ori, double c2, AxisOrientation c2ori) {
 			if (c1ori == c2ori) throw new ArgumentException($"Orientations are equal {c1ori}");
 			// only 2 combinations remain
 			if (c1ori == AxisOrientation.Horizontal && c2ori == AxisOrientation.Vertical) {
 				return (c1, c2);
 			}
-			else if (c1ori == AxisOrientation.Vertical && c2ori == AxisOrientation.Horizontal) {
+			else if (c2ori == AxisOrientation.Horizontal && c1ori == AxisOrientation.Vertical) {
 				return (c2, c1);
 			}
 			else {
