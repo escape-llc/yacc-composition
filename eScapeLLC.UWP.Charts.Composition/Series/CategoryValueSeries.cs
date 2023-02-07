5ec357254789e70bf6a418c716295f4594d5b12b
eScape LLC <winker9@hotmail.com>
2023-02-07 17:36:47 -0500

    big fat refactor; make generic on item state; push down data source handlers as virtual.

--- eScapeLLC.UWP.Charts.Composition/Series/CategoryValueSeries.cs
+++ eScapeLLC.UWP.Charts.Composition/Series/CategoryValueSeries.cs
@@ -3,11 +3,17 @@ using eScape.Host;
 using eScapeLLC.UWP.Charts.Composition.Events;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace eScapeLLC.UWP.Charts.Composition {
-	public abstract class CategoryValueSeries : ChartComponent,
-		IConsumer<Phase_ComponentExtents>, IConsumer<Axis_Extents>, IConsumer<Phase_DataSourceOperation> {
+	/// <summary>
+	/// Base class for series based on category/value axes.
+	/// Handles all the core data source operations and delegates to virtual/abstract methods.
+	/// </summary>
+	/// <typeparam name="S">Item type. MUST NOT be an Inner Class!</typeparam>
+	public abstract class CategoryValueSeries<S> : ChartComponent,
+		IConsumer<Phase_DataSourceOperation>, IConsumer<Phase_ComponentExtents>, IConsumer<Axis_Extents>, IConsumer<Phase_ModelComplete> where S: ItemStateCore {
 		static LogTools.Flag _trace = LogTools.Add("CategoryValueSeries", LogTools.Level.Error);
 		#region properties
 		/// <summary>
@@ -74,6 +80,20 @@ namespace eScapeLLC.UWP.Charts.Composition {
 		/// Obtain alternate label if not NULL.
 		/// </summary>
 		protected Binding LabelBinding { get; set; }
+		/// <summary>
+		/// Data needed for current state.
+		/// </summary>
+		protected List<ItemStateCore> ItemState { get; set; }
+		/// <summary>
+		/// MUST be NULL after processing.
+		/// This is because the "end" phases execute regardless of the <see cref="DataSource"/> that caused it.
+		/// </summary>
+		protected IEnumerable<(ItemStatus st, S state)> Current { get; set; }
+		#endregion
+		#region ctor
+		public CategoryValueSeries() {
+			ItemState = new List<ItemStateCore>();
+		}
 		#endregion
 		#region helpers
 		/// <summary>
@@ -95,12 +115,15 @@ namespace eScapeLLC.UWP.Charts.Composition {
 		}
 		/// <summary>
 		/// Reset the value and category limits to <see cref="double.NaN"/>.
-		/// Sets <see cref="ChartComponent.Dirty"/> = true.
 		/// </summary>
 		protected void ResetLimits() {
 			Component2Minimum = double.NaN; Component2Maximum = double.NaN;
 			Component1Minimum = double.NaN; Component1Maximum = double.NaN;
 		}
+		/// <summary>
+		/// Locate required components and generate errors if they are not found.
+		/// </summary>
+		/// <param name="iccc">Use to locate components and report errors.</param>
 		protected void EnsureAxes(IChartComponentContext iccc) {
 			IChartErrorInfo icei = iccc as IChartErrorInfo;
 			if (!string.IsNullOrEmpty(ValueAxisName)) {
@@ -132,6 +155,10 @@ namespace eScapeLLC.UWP.Charts.Composition {
 				icei?.Report(new ChartValidationResult(NameOrType(), $"Property '{nameof(CategoryAxisName)}' was not set", new[] { nameof(CategoryAxisName) }));
 			}
 		}
+		/// <summary>
+		/// Check that the required binding values are set and generate errors if not.
+		/// </summary>
+		/// <param name="iccc">Use to locate components and report errors.</param>
 		protected void EnsureValuePath(IChartComponentContext iccc) {
 			IChartErrorInfo icei = iccc as IChartErrorInfo;
 			if (string.IsNullOrEmpty(ValueMemberName)) {
@@ -141,54 +168,120 @@ namespace eScapeLLC.UWP.Charts.Composition {
 		#endregion
 		#region virtual data source handlers
 		/// <summary>
-		/// Reset exits any existing elements and enters all the elements given.
+		/// Fabricate state used for entering items.
+		/// </summary>
+		/// <param name="items">Item source.</param>
+		/// <param name="createstate">State factory method.</param>
+		/// <returns>New instance.</returns>
+		protected virtual IEnumerable<(ItemStatus st, S state)> Entering(System.Collections.IList items) {
+			for (int ix = 0; ix < items.Count; ix++) {
+				var state = CreateState(ix, items[ix]);
+				yield return (ItemStatus.Enter, state);
+			}
+		}
+		/// <summary>
+		/// Create the state needed for this series.
+		/// </summary>
+		/// <param name="index">Item index.</param>
+		/// <param name="item">Data item.</param>
+		/// <returns>New instance.</returns>
+		protected abstract S CreateState(int index, object item);
+		/// <summary>
+		/// Reset exits all existing elements and enters all the elements given.
 		/// </summary>
 		/// <param name="reset"></param>
-		protected virtual void Reset(DataSource_Reset reset) { }
+		protected virtual void Reset(DataSource_Reset reset) {
+			IEnumerable<(ItemStatus st, S state)> exit = ItemState.Select(xx => (ItemStatus.Exit, xx as S));
+			IEnumerable<(ItemStatus st, S state)> enter = Entering(reset.Items);
+			Current = exit.Concat(enter);
+		}
 		/// <summary>
-		/// Sliding window exits and enters same number of elements on front/rear respectively.
+		/// Sliding window exits and enters same number of elements on head/tail respectively.
 		/// </summary>
 		/// <param name="slidingWindow"></param>
-		protected virtual void SlidingWindow(DataSource_SlidingWindow slidingWindow) { }
+		protected virtual void SlidingWindow(DataSource_SlidingWindow slidingWindow) {
+			IEnumerable<(ItemStatus st, S state)> exit = ItemState.Take(slidingWindow.NewItems.Count).Select(xx => (ItemStatus.Exit, xx as S));
+			IEnumerable<(ItemStatus st, S state)> live = ItemState.Skip(slidingWindow.NewItems.Count).Select(xx => (ItemStatus.Live, xx as S));
+			IEnumerable<(ItemStatus st, S state)> enter = Entering(slidingWindow.NewItems);
+			Current = exit.Concat(live).Concat(enter);
+		}
 		/// <summary>
-		/// Add enters the given element at the given position.
+		/// Add enters the given element(s) at the given end.
 		/// </summary>
 		/// <param name="add"></param>
-		protected virtual void Add(DataSource_Add add) { }
+		protected virtual void Add(DataSource_Add add) {
+			IEnumerable<(ItemStatus st, S state)> live = ItemState.Select(xx => (ItemStatus.Live, xx as S));
+			IEnumerable<(ItemStatus st, S state)> enter = Entering(add.NewItems);
+			IEnumerable<(ItemStatus st, S state)> final = add.AtFront ? enter.Concat(live) : live.Concat(enter);
+			Current = final;
+		}
 		/// <summary>
-		/// Remove exits the given number of elements from front/rear as indicated.
+		/// Remove exits the given number of elements from indicated end.
 		/// </summary>
 		/// <param name="remove"></param>
-		protected virtual void Remove(DataSource_Remove remove) { }
+		protected virtual void Remove(DataSource_Remove remove) {
+			if (remove.AtFront) {
+				IEnumerable<(ItemStatus st, S state)> exit = ItemState.Take(remove.Count).Select(xx => (ItemStatus.Exit, xx as S));
+				IEnumerable<(ItemStatus st, S state)> live = ItemState.Skip(remove.Count).Select(xx => (ItemStatus.Live, xx as S));
+				Current = exit.Concat(live);
+			}
+			else {
+				IEnumerable<(ItemStatus st, S state)> live = ItemState.Take(ItemState.Count - remove.Count).Select(xx => (ItemStatus.Live, xx as S));
+				IEnumerable<(ItemStatus st, S state)> exit = ItemState.Skip(ItemState.Count - remove.Count).Select(xx => (ItemStatus.Exit, xx as S));
+				Current = live.Concat(exit);
+			}
+		}
 		/// <summary>
-		/// Respond to axis information update in <see cref="CategoryAxis"/> and <see cref="ValueAxis"/>.
+		/// Called during <see cref="Phase_ModelComplete"/>.
+		/// Apply axis information from <see cref="CategoryAxis"/> and <see cref="ValueAxis"/> to the Model transform.
 		/// </summary>
 		protected abstract void UpdateModelTransform();
+		/// <summary>
+		/// Called during <see cref="Phase_ModelComplete"/>.
+		/// Called after UpdateModelTransform().
+		/// Perform final layout of geometry; all axis extents are finalized.
+		/// </summary>
+		protected abstract void ModelComplete();
+		/// <summary>
+		/// Called during <see cref="Phase_ComponentExtents"/>.
+		/// Calculate component extents into XXXMinimum/XXXMaximum.
+		/// </summary>
+		protected abstract void ComponentExtents();
 		#endregion
 		#region handlers
 		/// <summary>
-		/// Respond with current series extents so axes can update.
+		/// Calculate and respond with current series extents so axes can update.
 		/// </summary>
 		/// <param name="message"></param>
 		void IConsumer<Phase_ComponentExtents>.Consume(Phase_ComponentExtents message) {
+			ComponentExtents();
 			message.Register(new Component_Extents(Name, DataSourceName, CategoryAxisName, Component1Minimum, Component1Maximum));
 			message.Register(new Component_Extents(Name, DataSourceName, ValueAxisName, Component2Minimum, Component2Maximum));
 		}
 		/// <summary>
 		/// Axis extents participate in the Model transform.
+		/// Cache extents for model complete.
 		/// </summary>
 		/// <param name="message"></param>
 		void IConsumer<Axis_Extents>.Consume(Axis_Extents message) {
 			if (message.AxisName == CategoryAxisName) {
 				CategoryAxis = message;
-				if (double.IsNaN(CategoryAxis.Minimum) || double.IsNaN(CategoryAxis.Maximum)) return;
-				UpdateModelTransform();
 			}
 			else if (message.AxisName == ValueAxisName) {
 				ValueAxis = message;
-				if (double.IsNaN(ValueAxis.Minimum) || double.IsNaN(ValueAxis.Maximum)) return;
-				UpdateModelTransform();
 			}
+		}
+		/// <summary>
+		/// Update model transform.  Signal model complete.
+		/// </summary>
+		/// <param name="message"></param>
+		void IConsumer<Phase_ModelComplete>.Consume(Phase_ModelComplete message) {
+			if (CategoryAxis == null) return;
+			if (double.IsNaN(CategoryAxis.Minimum) || double.IsNaN(CategoryAxis.Maximum)) return;
+			if (ValueAxis == null) return;
+			if (double.IsNaN(ValueAxis.Minimum) || double.IsNaN(ValueAxis.Maximum)) return;
+			UpdateModelTransform();
+			ModelComplete();
 		}
 		/// <summary>
 		/// Dispatch to virtual handlers.
