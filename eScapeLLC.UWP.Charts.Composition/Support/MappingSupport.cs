7658e8c5788ff24066dcd0f5e193e7d4caede38d
eScape LLC <winker9@hotmail.com>
2023-02-28 11:51:05 -0500

    add Opposite and ToVector.

--- eScapeLLC.UWP.Charts.Composition/Support/MappingSupport.cs
+++ eScapeLLC.UWP.Charts.Composition/Support/MappingSupport.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Numerics;
 
 namespace eScapeLLC.UWP.Charts.Composition {
 	public static class MappingSupport {
+		/// <summary>
+		/// Return the "opposite" orientation.
+		/// </summary>
+		/// <param name="ao">Candidate orientation.</param>
+		/// <returns>Opposite orientation.</returns>
+		public static AxisOrientation OppositeOf(AxisOrientation ao) => ao == AxisOrientation.Vertical ? AxisOrientation.Horizontal : AxisOrientation.Vertical;
 		/// <summary>
 		/// Map components to their respective cartesian axes.
 		/// Horizontal axis receives the X, Vertical axis receives the Y.
@@ -26,6 +33,18 @@ namespace eScapeLLC.UWP.Charts.Composition {
 				// won't get here...
 				throw new InvalidOperationException($"Cannot map components c1:{c1ori} c2{c2ori}");
 			}
+		}
+		/// <summary>
+		/// Helper to create a <see cref="Vector2"/> which is most common return value.
+		/// </summary>
+		/// <param name="c1">Component 1.</param>
+		/// <param name="c1ori">Component 1 axis orientation.</param>
+		/// <param name="c2">Component 2.</param>
+		/// <param name="c2ori">Component 2 axis orientation.</param>
+		/// <returns></returns>
+		public static Vector2 ToVector(double c1, AxisOrientation c1ori, double c2, AxisOrientation c2ori) {
+			var (xx, yy) = MapComponents(c1, c1ori, c2, c2ori);
+			return new Vector2((float)xx, (float)yy);
 		}
 	}
 }
