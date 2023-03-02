3967c893ca4da408c3a29b8824b03f0872959c41
eScape LLC <winker9@hotmail.com>
2023-03-02 08:20:33 -0500

    doc updates.

--- eScapeLLC.UWP.Charts.Composition/Axis/CategoryAxis.cs
+++ eScapeLLC.UWP.Charts.Composition/Axis/CategoryAxis.cs
@@ -5,10 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.ServiceModel.Channels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Data;
 
 namespace eScapeLLC.UWP.Charts.Composition {
--- eScapeLLC.UWP.Charts.Composition/Series/ColumnSeries.cs
+++ eScapeLLC.UWP.Charts.Composition/Series/ColumnSeries.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.Reflection;
 using Windows.Foundation;
 using Windows.UI.Composition;
 using Windows.UI.Xaml;
