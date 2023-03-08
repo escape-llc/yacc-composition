3f892dae4c20aae9645b500af585dacb38d44fa4
eScape LLC <winker9@hotmail.com>
2023-03-08 07:10:19 -0500

    rectangle geometry factory.

--- eScapeLLC.UWP.Charts.Composition/Series/ElementFactoryDefaults.cs
+++ eScapeLLC.UWP.Charts.Composition/Series/ElementFactoryDefaults.cs
@@ -4,6 +4,66 @@ using System.Numerics;
 using Windows.UI.Composition;
 
 namespace eScapeLLC.UWP.Charts.Composition.Factory {
+	#region RectangleGeometryFactory
+	/// <summary>
+	/// Default factory for creating rounded rectangle sprites.
+	/// </summary>
+	public class RectangleGeometryFactory : IElementFactory {
+		#region properties
+		public Style_Brush FillBrush { get; set; }
+		public Style_Brush StrokeBrush { get; set; }
+		public Style_Stroke Stroke { get; set; }
+		/// <summary>
+		/// Whether to flip linear gradients for negative value bars.
+		/// Use this when the gradient orients along the bar's axis, and it will "mirror" the gradients around the value axis zero.
+		/// </summary>
+		public bool FlipGradients { get; set; }
+		#endregion
+		public RectangleGeometryFactory() { }
+		/// <summary>
+		/// Swap the start and end points of gradient brush.
+		/// </summary>
+		/// <param name="blg"></param>
+		/// <param name="clgb"></param>
+		protected void FlipGradient(Brush_LinearGradient blg, CompositionLinearGradientBrush clgb) {
+			var flip = clgb.StartPoint;
+			clgb.StartPoint = clgb.EndPoint;
+			clgb.EndPoint = flip;
+		}
+		/// <summary>
+		/// <inheritdoc/>
+		/// Flips the gradient direction for negative bars.
+		/// </summary>
+		/// <param name="c"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		/// <param name="c1axis"></param>
+		/// <param name="c2axis"></param>
+		/// <returns></returns>
+		public CompositionShape CreateElement(IElementFactoryContext iefc) {
+			var rectangle = iefc.Compositor.CreateRectangleGeometry();
+			var sprite = iefc.Compositor.CreateSpriteShape(rectangle);
+			Stroke?.Apply(sprite);
+			var ierc = iefc as IElementRectangleContext;
+			if (FillBrush != null) {
+				sprite.FillBrush = FillBrush.CreateBrush(iefc.Compositor);
+				if (ierc.DataValue < 0 && FillBrush is Brush_LinearGradient blg && FlipGradients && sprite.FillBrush is CompositionLinearGradientBrush clgb) {
+					FlipGradient(blg, clgb);
+				}
+			}
+			if (StrokeBrush != null) {
+				sprite.StrokeBrush = StrokeBrush.CreateBrush(iefc.Compositor);
+				if (ierc.DataValue < 0 && StrokeBrush is Brush_LinearGradient blg && FlipGradients && sprite.StrokeBrush is CompositionLinearGradientBrush clgb) {
+					FlipGradient(blg, clgb);
+				}
+			}
+			// Offset and Size are Model units (input side of transform)
+			rectangle.Size = new Vector2((float)ierc.Width, (float)ierc.Height);
+			// Offset and Transform are managed by the caller
+			return sprite;
+		}
+	}
+	#endregion
 	#region RoundedRectangleGeometryFactory
 	/// <summary>
 	/// Default factory for creating rounded rectangle sprites.
