02c27eb7a2db1ac061f972cbc945dd36278c61b4
eScape LLC <winker9@hotmail.com>
2023-05-30 20:26:37 -0400

    doc update; refactor.

--- eScapeLLC.UWP.Charts.Composition/Series/AnimationProperties.cs
+++ eScapeLLC.UWP.Charts.Composition/Series/AnimationProperties.cs
@@ -93,16 +93,6 @@ namespace eScapeLLC.UWP.Charts.Composition.Animation {
 		/// <param name="cx">Use to acquire objects.</param>
 		/// <param name="name">Use for names.</param>
 		public Global(Compositor cx, string name) {
-			CreateAnimations(cx, name);
-		}
-		#endregion
-		#region extensions
-		/// <summary>
-		/// Initialize the property set.
-		/// </summary>
-		/// <param name="cx">Use to create objects.</param>
-		/// <param name="name">Use for names.</param>
-		protected virtual void CreateAnimations(Compositor cx, string name) {
 			PropertySet = cx.CreatePropertySet();
 			PropertySet.Comment = $"{name}_global";
 			PropertySet.InsertVector3(PROP_ModelX, new Vector3(1, 0, 0));
@@ -122,15 +112,17 @@ namespace eScapeLLC.UWP.Charts.Composition.Animation {
 		/// </summary>
 		/// <param name="model">Model transform.</param>
 		public virtual void Model(Matrix3x2 model) {
+			var a1 = new Vector3(model.M11, model.M21, model.M31);
+			var a2 = new Vector3(model.M12, model.M22, model.M32);
 			if (!isModelInit) {
-				PropertySet.InsertVector3(PROP_ModelX, new Vector3(model.M11, model.M21, model.M31));
-				PropertySet.InsertVector3(PROP_ModelY, new Vector3(model.M12, model.M22, model.M32));
+				PropertySet.InsertVector3(PROP_ModelX, a1);
+				PropertySet.InsertVector3(PROP_ModelY, a2);
 				isModelInit = true;
 			}
 			if (model == CurrentModel) return;
 			CurrentModel = model;
-			ModelX.SetVector3Parameter(AnimationProperties.PARM_Index, new Vector3(model.M11, model.M21, model.M31));
-			ModelY.SetVector3Parameter(AnimationProperties.PARM_Index, new Vector3(model.M12, model.M22, model.M32));
+			ModelX.SetVector3Parameter(AnimationProperties.PARM_Index, a1);
+			ModelY.SetVector3Parameter(AnimationProperties.PARM_Index, a2);
 			PropertySet.StartAnimation(ModelX.Target, ModelX);
 			PropertySet.StartAnimation(ModelY.Target, ModelY);
 		}
@@ -140,15 +132,17 @@ namespace eScapeLLC.UWP.Charts.Composition.Animation {
 		/// </summary>
 		/// <param name="proj">Projection transform.</param>
 		public virtual void Projection(Matrix3x2 proj) {
+			var a1 = new Vector3(proj.M11, proj.M21, proj.M31);
+			var a2 = new Vector3(proj.M12, proj.M22, proj.M32);
 			if (!isProjInit) {
-				PropertySet.InsertVector3(PROP_ProjX, new Vector3(proj.M11, proj.M21, proj.M31));
-				PropertySet.InsertVector3(PROP_ProjY, new Vector3(proj.M12, proj.M22, proj.M32));
+				PropertySet.InsertVector3(PROP_ProjX, a1);
+				PropertySet.InsertVector3(PROP_ProjY, a2);
 				isProjInit = true;
 			}
 			if (proj == CurrentProjection) return;
 			CurrentProjection = proj;
-			ProjX.SetVector3Parameter(AnimationProperties.PARM_Index, new Vector3(proj.M11, proj.M21, proj.M31));
-			ProjY.SetVector3Parameter(AnimationProperties.PARM_Index, new Vector3(proj.M12, proj.M22, proj.M32));
+			ProjX.SetVector3Parameter(AnimationProperties.PARM_Index, a1);
+			ProjY.SetVector3Parameter(AnimationProperties.PARM_Index, a2);
 			PropertySet.StartAnimation(ProjX.Target, ProjX);
 			PropertySet.StartAnimation(ProjY.Target, ProjY);
 		}
@@ -174,11 +168,118 @@ namespace eScapeLLC.UWP.Charts.Composition.Animation {
 		#endregion
 	}
 	#endregion
-	#region Local
+	#region ModelTransform
+	/// <summary>
+	/// Animates the Model for <see cref="CompositionShape.TransformMatrix"/>.
+	/// </summary>
+	public class ModelTransform : IDisposable {
+		public const string PROP_Component1 = "Component1";
+		public const string PROP_Component2 = "Component2";
+		private bool isModelInit;
+		private bool disposedValue;
+		#region properties
+		/// <summary>
+		/// Access to the last-updated model from <see cref="Model(Matrix3x2)"/>.
+		/// </summary>
+		public Matrix3x2 CurrentModel { get; protected set; }
+		/// <summary>
+		/// Access to property set hosting animated values.
+		/// </summary>
+		public CompositionPropertySet PropertySet { get; private set; }
+		Vector3KeyFrameAnimation Axis1 { get; set; }
+		Vector3KeyFrameAnimation Axis2 { get; set; }
+		ExpressionAnimation Transform { get; set; }
+		#endregion
+		#region ctor
+		public ModelTransform(Compositor cx, string name, int dur) {
+			Axis1 = cx.CreateVector3KeyFrameAnimation();
+			Axis1.InsertExpressionKeyFrame(1f, PROP_Component1);
+			Axis1.Duration = TimeSpan.FromMilliseconds(dur);
+			Axis1.StopBehavior = AnimationStopBehavior.SetToFinalValue;
+			Axis1.Target = PROP_Component1;
+			Axis1.Comment = PROP_Component1;
+			Axis2 = cx.CreateVector3KeyFrameAnimation();
+			Axis2.InsertExpressionKeyFrame(1f, PROP_Component2);
+			Axis2.Duration = TimeSpan.FromMilliseconds(dur);
+			Axis2.StopBehavior = AnimationStopBehavior.SetToFinalValue;
+			Axis2.Target = PROP_Component2;
+			Axis2.Comment = PROP_Component2;
+			PropertySet = cx.CreatePropertySet();
+			PropertySet.Comment = $"{name}_Axisprops";
+			PropertySet.InsertVector3(PROP_Component1, new Vector3(1, 0, 0));
+			PropertySet.InsertVector3(PROP_Component2, new Vector3(0, 1, 0));
+			// Transform is an expression animating the Matrix3x2 based on the axisprops
+			// entering components are connected to this animation
+			Transform = cx.CreateExpressionAnimation();
+			Transform.Expression = "Matrix3x2(props.Component1.X, props.Component2.X, props.Component1.Y, props.Component2.Y, props.Component1.Z, props.Component2.Z)";
+			Transform.SetExpressionReferenceParameter("props", PropertySet);
+			Transform.Target = nameof(CompositionShape.TransformMatrix);
+			Transform.Comment = $"{name}_TransformMatrix";
+		}
+		#endregion
+		#region public
+		/// <summary>
+		/// Manage animations for the model transform when it changes.
+		/// </summary>
+		/// <param name="model">New value for model; MAY equal previous value.</param>
+		public virtual void Model(Matrix3x2 model) {
+			var a1 = new Vector3(model.M11, model.M21, model.M31);
+			var a2 = new Vector3(model.M12, model.M22, model.M32);
+			if (!isModelInit) {
+				Axis1.SetVector3Parameter(PROP_Component1, a1);
+				Axis2.SetVector3Parameter(PROP_Component2, a2);
+				PropertySet.InsertVector3(PROP_Component1, a1);
+				PropertySet.InsertVector3(PROP_Component2, a2);
+				isModelInit = true;
+			}
+			if (model == CurrentModel) return;
+			CurrentModel = model;
+			Axis1.SetVector3Parameter(PROP_Component1, a1);
+			Axis2.SetVector3Parameter(PROP_Component2, a2);
+			PropertySet.StartAnimation(Axis1.Target, Axis1);
+			PropertySet.StartAnimation(Axis2.Target, Axis2);
+		}
+		/// <summary>
+		/// Start animation(s).
+		/// </summary>
+		/// <param name="co">Target object.</param>
+		public virtual void Start(CompositionObject co) {
+			co.StartAnimation(Transform.Target, Transform);
+		}
+		/// <summary>
+		/// Stop animation(s).
+		/// </summary>
+		/// <param name="co">Target object.</param>
+		public virtual void Stop(CompositionObject co) {
+			co.StopAnimation(Transform.Target);
+		}
+		#endregion
+		#region Dispose pattern
+		protected virtual void InternalDispose() {
+			Axis1?.Dispose(); Axis1 = null;
+			Axis2?.Dispose(); Axis2 = null;
+			PropertySet?.Dispose(); PropertySet = null;
+		}
+		void Dispose(bool disposing) {
+			if (!disposedValue) {
+				if (disposing) {
+					InternalDispose();
+				}
+				disposedValue = true;
+			}
+		}
+		public void Dispose() {
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+	#endregion
+	#region LocalVisual
 	/// <summary>
 	/// Core of the per-item composition animations.
 	/// </summary>
-	public class Local : IDisposable {
+	public class LocalVisual : IDisposable {
 		private bool disposedValue;
 		#region properties
 		public CompositionPropertySet PropertySet { get; private set; }
@@ -190,7 +291,7 @@ namespace eScapeLLC.UWP.Charts.Composition.Animation {
 		/// </summary>
 		/// <param name="cx">Use for obtaining objects.</param>
 		/// <param name="global">Global property set.</param>
-		public Local(Compositor cx, CompositionPropertySet global) {
+		public LocalVisual(Compositor cx, CompositionPropertySet global) {
 			if (cx == null) throw new ArgumentNullException(nameof(cx));
 			if (global == null) throw new ArgumentNullException(nameof(global));
 			Position = cx.CreateVector2KeyFrameAnimation();
@@ -270,19 +371,25 @@ namespace eScapeLLC.UWP.Charts.Composition.Animation {
 	#region MarkerBrush
 	/// <summary>
 	/// Maintains animations for a "Marker Brush" which is a rectangle placed at the World Coordinates.
-	/// The Marker Width is in Category Units and is the basis for the size calculations.
+	/// The Marker Width/Height is in Category Units and is the basis for the size calculations.
 	/// </summary>
-	public class MarkerBrush : Local {
+	public class MarkerBrush : LocalVisual {
 		/// <summary>
-		/// Name of property to place in GLOBAL property set.
+		/// Name of property to place in GLOBAL property set for C1 Horizontal.
 		/// </summary>
 		public const string PROP_MarkerWidth = "MarkerWidth";
+		/// <summary>
+		/// Name of property to place in GLOBAL property set for C1 Vertical.
+		/// </summary>
+		public const string PROP_MarkerHeight = "MarkerHeight";
 		/// <summary>
 		/// Name of property to place in GLOBAL property set.
 		/// </summary>
 		public const string PROP_AspectRatio = "AspectRatio";
-		readonly static string WIDTH = $"{AnimationProperties.PARM_global}.MarkerWidth*{AnimationProperties.PARM_global}.ModelX.X*{AnimationProperties.PARM_global}.ProjX.X";
-		readonly static string SIZE = $"Vector2({WIDTH}, ({WIDTH})*{AnimationProperties.PARM_global}.AspectRatio)";
+		readonly static string C1_HORZ = $"{AnimationProperties.PARM_global}.MarkerWidth*{AnimationProperties.PARM_global}.ModelX.X*{AnimationProperties.PARM_global}.ProjX.X";
+		readonly static string C1_VERT = $"{AnimationProperties.PARM_global}.MarkerHeight*{AnimationProperties.PARM_global}.ModelY.Y*{AnimationProperties.PARM_global}.ProjY.Y";
+		readonly static string SIZE_HORZ = $"Vector2({C1_HORZ}, ({C1_HORZ})*{AnimationProperties.PARM_global}.AspectRatio)";
+		readonly static string SIZE_VERT = $"Vector2(({C1_VERT})*{AnimationProperties.PARM_global}.AspectRatio, {C1_VERT})";
 		#region properties
 		ExpressionAnimation Offset { get; set; }
 		ExpressionAnimation Size { get; set; }
@@ -299,7 +406,7 @@ namespace eScapeLLC.UWP.Charts.Composition.Animation {
 			Offset.SetExpressionReferenceParameter(AnimationProperties.PARM_local, PropertySet);
 			Offset.SetExpressionReferenceParameter(AnimationProperties.PARM_global, global);
 			Offset.Target = nameof(Visual.Offset);
-			Size = cx.CreateExpressionAnimation(SIZE);
+			Size = cx.CreateExpressionAnimation(SIZE_HORZ);
 			Size.Comment = $"Marker[{GetHashCode()}]_Size";
 			Size.SetExpressionReferenceParameter(AnimationProperties.PARM_local, PropertySet);
 			Size.SetExpressionReferenceParameter(AnimationProperties.PARM_global, global);
@@ -332,7 +439,7 @@ namespace eScapeLLC.UWP.Charts.Composition.Animation {
 	/// <summary>
 	/// Manages the <see cref="Windows.UI.Xaml.UIElement.Translation"/> property instead of <see cref="Visual.Offset"/> to animate a <see cref="Windows.UI.Xaml.UIElement"/>.
 	/// </summary>
-	public class XamlElement : Local {
+	public class XamlElement : LocalVisual {
 		#region properties
 		ExpressionAnimation Translate { get; set; }
 		#endregion
